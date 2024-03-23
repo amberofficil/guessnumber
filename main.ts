b45d0dd8189b6746a3533a3b7c0ef58233252e88
amber officil <amberofficial380@gmail.com>
2024-03-24 03:55:18 +0500

    preparinggame

---
+++ main.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+
+console.log("guess a number between 1 to 10");
+//   now varible creat
+let a:number=Math.floor(Math.random()*10)
+import inquirer from "inquirer";
+while(true){
+    let input=await inquirer.prompt
+    ({
+        name:"guess", type:"number",
+        message:"enter your guess number you want:  " 
+    })
+    
+    //  provide code
+    let ans:number= input.guess
+    if (a==ans)
+    {console.log("Congratulation your guess number absolutely correct")
+break;}
+else{console.log("Sorry you guess wrong number try again ")}
+}
